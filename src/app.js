5edcb35aba22274296c2bbc6dee355a749bab158
harshsingh1805 <harshvardhansingh.is22@bmsce.ac.in>
2025-12-20 20:18:11 +0530

    added logs-middleware-service

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+app.use(helmet());
+app.use(cors());
+app.use(
+  morgan('combined', { stream: { write: msg => logger.info(msg.trim()) } })
+);
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
+  logger.info('Hello from Acquisitions !');
   res.status(200).send('Hello from Acquisitions !');
 });
 
